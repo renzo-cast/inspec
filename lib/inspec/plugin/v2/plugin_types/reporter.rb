268b328feb783596e300302d45e7df1d74c526b5
Clinton Wolfe <clintoncwolfe@gmail.com>
2020-05-07 00:42:51 -0400

    Add an object model for run_data
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/inspec/plugin/v2/plugin_types/reporter.rb
+++ lib/inspec/plugin/v2/plugin_types/reporter.rb
@@ -1,3 +1,5 @@
+require_relative "../../../run_data"
+
 module Inspec::Plugin::V2::PluginType
   class Reporter < Inspec::Plugin::V2::PluginBase
     register_plugin_type(:reporter)
@@ -6,7 +8,12 @@ module Inspec::Plugin::V2::PluginType
 
     def initialize(config)
       @config = config
-      @run_data = config[:run_data]
+      unless Inspec::RunData.compatible_schema?(self.class.run_data_schema_constraints)
+        # Best we can do is warn here, the InSpec run has finished
+        # TODO: one day, perhaps switch RunData implementations to try to satisfy constraints?
+        Inspec::Log.warn "Reporter does not support RunData API (#{Inspec::RunData::SCHEMA_VERSION}), Reporter constraints: '#{self.class.run_data_schema_constraints}'"
+      end
+      @run_data = Inspec::RunData.new(config[:run_data])
       @output = ""
     end
 
@@ -22,6 +29,10 @@ module Inspec::Plugin::V2::PluginType
     # each reporter must implement #render
     def render
       raise NotImplementedError, "#{self.class} must implement a `#render` method to format its output."
+    end
+
+    def self.run_data_schema_constraints
+      raise NotImplementedError, "#{self.class} must implement a `run_data_schema_constraints` class method to declare its compatibiltity with the RunData API."
     end
   end
 end
